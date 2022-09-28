455e819f8e22a944df99fc9421d671dec017c2cc
Samb <samb@townsware.com>
2022-09-28 19:55:44 -0400

    SB

--- uberpickupexample.py
+++ uberpickupexample.py
@@ -6,10 +6,10 @@ import pandas as pd
 import numpy as np
 
 st.title('Uber pickups in NYC')
-st.txt('Sam Boutros')
-st.txt('FHSU - Fall 2022 - INF601 Advanced Python')
-st.txt('Prof. Jason Zeller')
-st.txt('Week 6 - Streamlit practice')
+st.text('Sam Boutros')
+st.text('FHSU - Fall 2022 - INF601 Advanced Python')
+st.text('Prof. Jason Zeller')
+st.text('Week 6 - Streamlit practice')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
